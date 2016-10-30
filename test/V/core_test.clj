e8474fa873236fe80eeab5ac894d0087451406ab
Chris Ford <christophertford@gmail.com>
2016-10-30 17:34:27 +0000

    Avoid matching against the implementation in the integration tests.

--- test/V/core_test.clj
+++ test/V/core_test.clj
@@ -36,7 +36,7 @@
 
 (defn parse-interval [text]
   (let [value (v/success text)
-        json ((v/lift load-string) value)
+        json ((v/exception->error load-string [:json :invalid]) value)
         start ((v/extract :start [:start :missing]) json)
         end ((v/extract :end [:end :missing]) json)
         interval ((v/lift list) (parse-date :start start) (parse-date :end end))
@@ -44,13 +44,15 @@
     ((v/check before [:interval :invalid]) interval)))
 
 (deftest integration
-  (is (= {:value [(java.util.Date. 116 2 3) (java.util.Date. 116 3 4)]}
+  (is (= (v/success [(java.util.Date. 116 2 3) (java.util.Date. 116 3 4)])
          (parse-interval "{:start {:day 3 :month 2 :year 2016} :end {:day 4 :month 3 :year 2016}}")))
-  (is (= {:errors #{[:start :missing-day] [:start :missing-year] [:end :missing]}}
+  (is (= (v/failure [:json :invalid])
+         (parse-interval "asdfasdfasd")))
+  (is (= (v/failure [:start :missing-day] [:start :missing-year] [:end :missing])
          (parse-interval "{:start {:month 2}}")))
-  (is (= {:errors #{[:start :bad-date] [:end :missing]}}
+  (is (= (v/failure [:start :bad-date] [:end :missing])
          (parse-interval "{:start {:day 3 :month \"Foo\" :year 2016}}")))
-  (is (= {:errors #{[:interval :invalid]}}
+  (is (= (v/failure [:interval :invalid])
          (parse-interval "{:start {:day 3 :month 4 :year 2016} :end {:day 4 :month 3 :year 2016}}"))))
 
 (lifting)
