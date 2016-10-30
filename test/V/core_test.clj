212f3955ea3acb76d21d5eb346c1cf3c88da19e5
Chris Ford <christophertford@gmail.com>
2016-10-30 21:44:49 +0000

    Avoid exposing the internals to the unit tests.

--- test/V/core_test.clj
+++ test/V/core_test.clj
@@ -4,22 +4,22 @@
     [V.core :as v]))
 
 (deftest lifting
-  (is (= {:value 3} (v/lift + (v/success 1) (v/success 2))))
-  (is (= {:errors #{":-("}} (v/lift + (v/failure ":-(") (v/success 2))))
-  (is (= {:errors #{":-("}} (v/lift + (v/success 1) (v/failure ":-("))))
-  (is (= {:errors #{":-|" ":-/" ":-("}} (v/lift + (v/failure ":-/") (v/failure ":-(") (v/failure ":-|")))))
+  (is (= (v/success 3) (v/lift + (v/success 1) (v/success 2))))
+  (is (= (v/failure ":-(") (v/lift + (v/failure ":-(") (v/success 2))))
+  (is (= (v/failure ":-(") (v/lift + (v/success 1) (v/failure ":-("))))
+  (is (= (v/failure ":-|" ":-/" ":-(") (v/lift + (v/failure ":-/") (v/failure ":-(") (v/failure ":-|")))))
 
 (deftest checking
-  (is (= {:errors #{"Odd"}} (v/check even? "Odd" (v/success 1))))
-  (is (= {:value 2} (v/check even? "Odd" (v/success 2))))
-  (is (= {:errors #{":-("}} (v/check even? "Odd" (v/failure ":-("))))
-  (is (= {:value 0} (v/check zero? :non-zero even? :odd (v/success 0))))
-  (is (= {:errors #{:non-zero}} (v/check zero? :non-zero even? :odd (v/success 4))))
-  (is (= {:errors #{:non-zero :odd}} (v/check zero? :non-zero even? :odd (v/success 3)))))
+  (is (= (v/failure "Odd") (v/check even? "Odd" (v/success 1))))
+  (is (= (v/success 2) (v/check even? "Odd" (v/success 2))))
+  (is (= (v/failure ":-(") (v/check even? "Odd" (v/failure ":-("))))
+  (is (= (v/success 0) (v/check zero? :non-zero even? :odd (v/success 0))))
+  (is (= (v/failure :non-zero) (v/check zero? :non-zero even? :odd (v/success 4))))
+  (is (= (v/failure :non-zero :odd) (v/check zero? :non-zero even? :odd (v/success 3)))))
 
 (deftest trying
-  (is (= {:errors #{"Couldn't parse."}} (v/catch-exceptions #(Integer/parseInt %) "Couldn't parse." (v/success "foo"))))
-  (is (= {:value 8} (v/catch-exceptions #(Integer/parseInt %) "Couldn't parse." (v/success "8")))))
+  (is (= (v/failure "Couldn't parse.") (v/catch-exceptions #(Integer/parseInt %) "Couldn't parse." (v/success "foo"))))
+  (is (= (v/success 8) (v/catch-exceptions #(Integer/parseInt %) "Couldn't parse." (v/success "8")))))
 
 (lifting)
 (checking)
