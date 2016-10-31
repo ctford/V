13b1ce4836dce7026c3a30308098a321d5e11f6b
Chris Ford <christophertford@gmail.com>
2016-10-31 08:44:46 +0000

    Rearrange example, hopefully for clarity.

--- test/V/core_integration.clj
+++ test/V/core_integration.clj
@@ -4,21 +4,20 @@
     [V.core :as v]))
 
 (defn parse-date [m k]
-  (let [day (v/extract :day [k :missing-day] m)
-        month (v/extract :month [k :missing-month] m)
-        year (v/extract :year [k :missing-year] m)
-        adjust (partial v/fmap #(- % 1900))
-        date (partial v/catch-exceptions #(java.util.Date. %1 %2 %3) [k :bad-date])]
-    (date (adjust year) month day)))
+  (let [day (->> m (v/extract :day [k :missing-day]))
+        month (->> m (v/extract :month [k :missing-month]))
+        year (->> m (v/extract :year [k :missing-year]) (v/fmap #(- % 1900)))
+        date (fn [y m d] (v/catch-exceptions #(java.util.Date. %1 %2 %3) [k :bad-date] y m d))]
+    (date year month day)))
 
 (defn parse-interval [text]
-  (let [value (v/success text)
-        json (v/catch-exceptions load-string [:json :invalid] value)
-        start (v/extract :start [:start :missing] json)
-        end (->> json (v/extract :end [:end :missing]) (v/default {:day 1 :month 1 :year 2017}))
-        interval (v/fmap list (parse-date start :start) (parse-date end :end))
-        starts-before-ends #(.before (first %) (second %))]
-    (v/check starts-before-ends [:interval :invalid] interval)))
+  (let [json (->> text v/success (v/catch-exceptions load-string [:json :invalid]))
+        start-map (->> json (v/extract :start [:start :missing]))
+        end-map (->> json (v/extract :end [:end :missing]) (v/default {:day 1 :month 1 :year 2017}))
+        interval (fn [s e]
+                   (->> (v/fmap vector s e)
+                        (v/check #(.before (first %) (second %)) [:interval :invalid])))]
+    (interval (parse-date start-map :start) (parse-date end-map :end))))
 
 (deftest integration
   (testing "Happy path"
