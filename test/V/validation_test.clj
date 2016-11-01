334326f3c6bc41c74957981935ae366fed1ab47e
Chris Ford <christophertford@gmail.com>
2016-11-01 20:04:21 +0000

    v/check-not is more useful than v/check-nil?.

--- test/V/validation_test.clj
+++ test/V/validation_test.clj
@@ -13,9 +13,9 @@
   (is (= (v/failure "Odd") (v/check even? "Odd" (v/success 1))))
   (is (= (v/success 2) (v/check even? "Odd" (v/success 2))))
   (is (= (v/failure ":-(") (v/check even? "Odd" (v/failure ":-("))))
-  (is (= (v/success 0) (v/check zero? :non-zero even? :odd (v/success 0))))
-  (is (= (v/failure :non-zero) (v/check zero? :non-zero even? :odd (v/success 4))))
-  (is (= (v/failure :non-zero :odd) (v/check zero? :non-zero even? :odd (v/success 3)))))
+  (is (= (v/success 1) (v/check-not nil? :whoops (v/success 1))))
+  (is (= (v/failure :whoops) (v/check-not nil? :whoops (v/success nil))))
+  (is (= (v/failure :yikes) (v/check-not nil? :whoops (v/failure :yikes)))))
 
 (deftest trying
   (is (= (v/failure "Couldn't parse.")
@@ -25,11 +25,6 @@
         (v/catch-exception NullPointerException #(Integer/parseInt %) "Couldn't parse." (v/success "foo"))))
   (is (= (v/success 8)
          (v/catch-exception NumberFormatException #(Integer/parseInt %) "Couldn't parse." (v/success "8")))))
-
-(deftest nil-checking
-  (is (= (v/success 1) (v/check-not nil? :whoops (v/success 1))))
-  (is (= (v/failure :whoops) (v/check-not nil? :whoops (v/success nil))))
-  (is (= (v/failure :yikes) (v/check-not nil? :whoops (v/failure :yikes)))))
 
 (deftest extracting
   (is (= (v/success 3) (v/extract :x :whoops (v/success {:x 3 :y 8}))))
@@ -44,6 +39,5 @@
 (fmapping)
 (checking)
 (trying)
-(nil-checking)
 (extracting)
 (defaulting)
