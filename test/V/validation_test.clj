3a1681e26b93594c06ac4d8133a4aefff07e5fde
Chris Ford <christophertford@gmail.com>
2016-11-02 21:02:31 +0000

    Use arrows rather than nesting.

--- test/V/validation_test.clj
+++ test/V/validation_test.clj
@@ -10,12 +10,12 @@
   (is (= (v/failure ":-|" ":-/" ":-(") (v/fmap + (v/failure ":-/") (v/failure ":-(") (v/failure ":-|")))))
 
 (deftest checking
-  (is (= (v/failure "Odd") (v/check even? "Odd" (v/success 1))))
-  (is (= (v/success 2) (v/check even? "Odd" (v/success 2))))
-  (is (= (v/failure ":-(") (v/check even? "Odd" (v/failure ":-("))))
-  (is (= (v/success 1) (v/check-not nil? :whoops (v/success 1))))
-  (is (= (v/failure :whoops) (v/check-not nil? :whoops (v/success nil))))
-  (is (= (v/failure :yikes) (v/check-not nil? :whoops (v/failure :yikes)))))
+  (is (= (v/failure "Odd") (->> (v/success 1) (v/check even? "Odd"))))
+  (is (= (v/success 2) (->> (v/success 2) (v/check even? "Odd"))))
+  (is (= (v/failure ":-(") (->> (v/failure ":-(") (v/check even? "Odd"))))
+  (is (= (v/success 1) (->> (v/success 1) (v/check-not nil? :whoops))))
+  (is (= (v/failure :whoops) (->> (v/success nil) (v/check-not nil? :whoops))))
+  (is (= (v/failure :yikes) (->> (v/failure :yikes) (v/check-not nil? :whoops)))))
 
 (deftest combining
   (is (= (v/success 0)
@@ -46,14 +46,14 @@
          (v/catch-exception NumberFormatException #(Integer/parseInt %) "Couldn't parse." (v/success "8")))))
 
 (deftest extracting
-  (is (= (v/success 3) (v/extract :x :whoops (v/success {:x 3 :y 8}))))
-  (is (= (v/success 3) (v/extract first :whoops (v/success [3 4 5]))))
-  (is (= (v/failure :yikes) (v/extract :x :whoops (v/failure :yikes))))
-  (is (= (v/failure :whoops) (v/extract :z :whoops (v/success {:x 3 :y 8})))))
+  (is (= (v/success 3) (->> (v/success {:x 3 :y 8}) (v/extract :x :whoops))))
+  (is (= (v/success 3) (->> (v/success [3 4 5]) (v/extract first :whoops))))
+  (is (= (v/failure :yikes) (->> (v/failure :yikes) (v/extract :x :whoops))))
+  (is (= (v/failure :whoops) (->> (v/success {:x 3 :y 8}) (v/extract :z :whoops)))))
 
 (deftest defaulting
-  (is (= (v/success 5) (v/default 5 (v/failure :whoops))))
-  (is (= (v/success 6) (v/default (v/success 5) (v/success 6)))))
+  (is (= (v/success 5) (->> (v/failure :whoops) (v/default 5))))
+  (is (= (v/success 6) (->> (v/success 6) (v/default 5)))))
 
 (fmapping)
 (checking)
