c60b71aa17e1d041c8c65d20adc113ddf84e7be4
Chris Ford <christophertford@gmail.com>
2016-11-02 10:54:19 +0000

    Test that numbers are in range.

--- test/V/validation_integration.clj
+++ test/V/validation_integration.clj
@@ -6,10 +6,17 @@
 (defn date [y m d]
   (java.util.Date. y m d))
 
+(defn between? [a b]
+  (fn [x] (and (number? x) (<= a x b))))
+
 (defn parse-date [k m]
-  (let [day   (->> m (v/extract :day   [k :missing-day]))
-        month (->> m (v/extract :month [k :missing-month]))
-        year  (->> m (v/extract :year  [k :missing-year]) (v/fmap #(- % 1900)))]
+  (let [day   (->> m (v/extract :day           [k :missing-day])
+                     (v/check (between? 1 32)  [k :bad-date]))
+        month (->> m (v/extract :month         [k :missing-month])
+                     (v/check (between? 1 13)  [k :bad-date]))
+        year  (->> m (v/extract :year          [k :missing-year])
+                     (v/fmap #(- % 1900))
+                     (v/check (between? 1 117) [k :bad-date]))]
     (v/catch-exception ClassCastException date [k :bad-date] year month day)))
 
 (defn parse-interval [text]
@@ -21,7 +28,7 @@
         end   (->> json
                    (v/extract :end [:end :missing])
                    (v/default {:day 1 :month 1 :year 2017})
-                   (parse-date :start))]
+                   (parse-date :end))]
     (->> (v/fmap vector start end)
          (v/check #(.before (first %) (second %)) [:interval :invalid]))))
 
@@ -37,6 +44,8 @@
     (is (= (v/failure [:start :bad-date])
            (parse-interval "{:start {:day 3 :month \"Foo\" :year 2016}}")))
     (is (= (v/failure [:interval :invalid])
-           (parse-interval "{:start {:day 3 :month 4 :year 2016} :end {:day 4 :month 3 :year 2016}}")))))
+           (parse-interval "{:start {:day 3 :month 4 :year 2016} :end {:day 4 :month 3 :year 2016}}")))
+    (is (= (v/failure [:start :bad-date] [:end :bad-date])
+           (parse-interval "{:start {:day -3 :month 2 :year 2016} :end {:day 44 :month 3 :year 2016}}")))))
 
 (integration)
