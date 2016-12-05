ce60c971f5b57051ee7116af1536bbebeda24c9b
Chris Ford <christophertford@gmail.com>
2016-12-05 22:34:28 +0000

    Define local gets to make extraction neater.

--- test/V/validation_integration.clj
+++ test/V/validation_integration.clj
@@ -12,21 +12,23 @@
         day?   (v/check (within? 1 31)      [k :bad-date])
         month? (v/check (within? 1 12)      [k :bad-date])
         year?  (v/check (within? 1900 2017) [k :bad-year])
-        day    (-> m (v/extract :day        [k :missing-day])   day?)
-        month  (-> m (v/extract :month      [k :missing-month]) month?)
-        year   (-> m (v/extract :year       [k :missing-year])  year?)]
+        get (fn [m field] (v/extract m field [k :missing field]))
+        day    (-> m (get :day) day?)
+        month  (-> m (get :month) month?)
+        year   (-> m (get :year) year?)]
     (date (- year 1900) month day)))
 
 (defn parse-interval [text]
   (let [vector (v/fmap vector)
         load-string (v/catch-exception RuntimeException load-string [:json :invalid])
         in-order? (v/check #(.before (first %) (second %)) [:interval :invalid])
+        get (fn [m field] (v/extract m field [field :missing]))
         json  (load-string text)
         start (-> json
-                  (v/extract  :start [:start :missing])
+                  (get :start)
                   (parse-date :start))
         end   (-> json
-                  (v/extract  :end [:end :missing])
+                  (get :end)
                   (v/default {:day 1 :month 1 :year 2017})
                   (parse-date :end))]
     (-> (vector start end) in-order?)))
@@ -38,7 +40,7 @@
   (testing "Sad paths"
     (is (= (v/failure [:json :invalid])
            (parse-interval "asdfasdfasd")))
-    (is (= (v/failure [:start :missing-day] [:start :missing-year])
+    (is (= (v/failure [:start :missing :day] [:start :missing :year])
            (parse-interval "{:start {:month 2}}")))
     (is (= (v/failure [:start :bad-date])
            (parse-interval "{:start {:day 3 :month \"Foo\" :year 2016}}")))
