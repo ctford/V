8e311489c399d7946345f1a088da1bea6d17f943
Chris Ford <christophertford@gmail.com>
2016-12-09 17:46:27 +0000

    Format test cases and add a nil one.

--- test/V/validation_integration.clj
+++ test/V/validation_integration.clj
@@ -36,19 +36,30 @@
     (-> (vector start end) in-order?)))
 
 (deftest integration
+
   (testing "Happy path"
-    (is (= [(java.util.Date. 116 2 3) (java.util.Date. 116 3 4)]
-           (parse-interval "{:start {:day 3 :month 2 :year 2016} :end {:day 4 :month 3 :year 2016}}"))))
+
+    (is (= (parse-interval "{:start {:day 3 :month 2 :year 2016} :end {:day 4 :month 3 :year 2016}}")
+           [(java.util.Date. 116 2 3) (java.util.Date. 116 3 4)])))
+
   (testing "Sad paths"
-    (is (= (v/failure [:json :invalid])
-           (parse-interval "asdfasdfasd")))
-    (is (= (v/failure [:start :missing :day] [:start :missing :year])
-           (parse-interval "{:start {:month 2}}")))
-    (is (= (v/failure [:start :bad-date])
-           (parse-interval "{:start {:day 3 :month \"Foo\" :year 2016}}")))
-    (is (= (v/failure [:interval :invalid])
-           (parse-interval "{:start {:day 3 :month 4 :year 2016} :end {:day 4 :month 3 :year 2016}}")))
-    (is (= (v/failure [:start :bad-date] [:end :bad-date])
-           (parse-interval "{:start {:day -3 :month 2 :year 2016} :end {:day 44 :month 3 :year 2016}}")))))
+
+    (is (= (parse-interval nil)
+           (v/failure [:json :invalid])))
+
+    (is (= (parse-interval "asdfasdfasd")
+           (v/failure [:json :invalid])))
+
+    (is (= (parse-interval "{:start {:month 2}}")
+           (v/failure [:start :missing :day] [:start :missing :year])))
+
+    (is (= (parse-interval "{:start {:day 3 :month \"Foo\" :year 2016}}")
+           (v/failure [:start :bad-date])))
+
+    (is (= (parse-interval "{:start {:day 3 :month 4 :year 2016} :end {:day 4 :month 3 :year 2016}}")
+           (v/failure [:interval :invalid])))
+
+    (is (= (parse-interval "{:start {:day -3 :month 2 :year 2016} :end {:day 44 :month 3 :year 2016}}")
+          (v/failure [:start :bad-date] [:end :bad-date])))))
 
 (integration)
